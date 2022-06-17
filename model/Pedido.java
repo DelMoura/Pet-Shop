a3d323eea867162c33832f613d70ed29f1f9460a
DelMoura <slitherhead380@gmail.com>
2022-06-17 01:31:42 -0300

    Segunda versão

---
+++ model/Pedido.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Pedido {
+    private Animal animal;
+    private Cliente cliente;
+    //private Procedimento procedimento; //Deverá criar a classe Procedimento
+    
+    public Animal getAnimal() {
+        return animal;
+    }
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    //public void inserirSacola(){}
+    //public Sacola totalVendido(){}
+    
+
+    
+}
